97a55dea8b46c9a50d4ea59eca784e36ae0cc8d3
anarchymonkey <aniketbatabyal1996@gmail.com>
2023-09-17 22:33:57 +0530

    added docker compose file for all three projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
+import { useRef, useState } from "react";
+import axios from 'axios';
+
 
 
 export const App = () => {
+    const [shortURL, setShortUrl] = useState(null);
+    const longURLRef = useRef();
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log({ longUrl: longURLRef.current.value })
+        
+        axios.post("http://localhost:8080/generate-short-url", {
+            longURL: longURLRef.current.value,
+        }).then(resp => {
+            setShortUrl(resp.data.shortURL)
+        }).catch(err => {
+            alert(err)
+        })
+        
+    }
     return (
         <div>
-            This is our dummy application
+            <form onSubmit={handleSubmit}>
+                <input type="textbox" name="longURL" id="longURL" ref={longURLRef}  />
+                <input type="submit" name="submit" placeholder="submit" />
+            </form>
+            <a href={shortURL}>{shortURL} </a>
         </div>
     )
 }
